6a0d2b7cdf7276989c3133f91c23622bdc5fa115
Olga Demina <miserylab.r6s@gmail.com>
2023-03-05 22:43:51 +0300

    init commit

---
+++ lessons/lesson2/lesson2.py
@@ -0,0 +1,16 @@
+from json.decoder import JSONDecodeError
+import requests
+
+# response = requests.get("https://playground.learnqa.ru/api/hello", params={"name":"User"})
+# parsed_response_text = response.json()
+# print(parsed_response_text["answer"])
+
+
+response = requests.get("https://playground.learnqa.ru/api/get_text")
+print(response.text)
+
+try:
+    parsed_response_text = response.json()
+    print(parsed_response_text)
+except JSONDecodeError:
+    print("Response is not a JSON format")
